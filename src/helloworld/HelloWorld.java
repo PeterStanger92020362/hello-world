efb396094c54b39074747923625b09df8c60231a
Peter Stanger <psta191@mywhitecliffe.com>
2020-05-12 20:57:31 +1200

    added an extra line of output

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -15,7 +15,8 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World 2.0");
+        System.out.println("Hello World");
+        System.out.println("Check out my latest commit!");
     }
     
 }
